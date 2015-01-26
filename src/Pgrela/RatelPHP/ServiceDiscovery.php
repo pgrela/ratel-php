d7292ac429699fd102732ed7edcb6ce25c3d72c9
Patryk Grela <patryk.grela@gmail.com>
2015-01-26 20:22:15 +0100

    Hessian type map support

--- src/Pgrela/RatelPHP/ServiceDiscovery.php
+++ src/Pgrela/RatelPHP/ServiceDiscovery.php
@@ -2,24 +2,34 @@
 
 namespace Pgrela\RatelPHP;
 
-class ServiceDiscovery {
+use HessianOptions;
+
+class ServiceDiscovery
+{
     const DS = '/';
     const SERVICES_ROOT = '/services';
     private $services;
     private $zookeeper;
+    private $hessianOptions;
 
 
-    public function __construct($zkHost){
+    public function __construct($zkHost, $typeMap = array())
+    {
         $this->zookeeper = new \Zookeeper($zkHost);
         $this->services = $this->zookeeper->getChildren(self::SERVICES_ROOT);
+
+        $this->hessianOptions = new HessianOptions();
+        $this->hessianOptions->typeMap = $typeMap;
     }
-    public function getClient($interface){
-        if(in_array($interface,$this->services)) {
+
+    public function getClient($interface)
+    {
+        if (in_array($interface, $this->services)) {
             $interfacePath = self::SERVICES_ROOT . self::DS . $interface;
             $instances = $this->zookeeper->getChildren($interfacePath);
-            $instance = $instances[rand(0,count($instances)-1)];
-            $host = json_decode($this->zookeeper->get($interfacePath.self::DS.$instance));
-            return new \HessianClient($host->address);
+            $instance = $instances[rand(0, count($instances) - 1)];
+            $host = json_decode($this->zookeeper->get($interfacePath . self::DS . $instance));
+            return new \HessianClient($host->address, $this->hessianOptions);
         }
     }
 }
